254abef1ce6ad5a21f9c89501e78898c8dfea246
ozyx <jessemazzella@gmail.com>
2017-03-10 14:59:48 -0800

    try jekyll serve as a prestart hook

--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,8 @@
 /// <reference types="electron" />
 import { app, BrowserWindow, Menu, Tray, autoUpdater, dialog } from "electron";
+
 const version = require("../package.json").version;
+
 
 // set application title
 let appTitle = `Desktop Grimoire v${version}`;
@@ -15,7 +17,9 @@ app.on("ready", () => {
         backgroundColor: "#2e2c29",
         title: app.getName()
     });
+
     mainWindow.once("ready-to-show", () => {
+        mainWindow.loadURL("localhost:4000");
         mainWindow.show();
         mainWindow.focus();
     });

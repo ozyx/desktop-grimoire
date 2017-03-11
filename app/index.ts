9ff8ca5dcdb6bc7031b6ec07d7b66d28a002c16a
ozyx <jessemazzella@gmail.com>
2017-03-10 16:13:02 -0800

    build and launch server as prestart hook

--- app/index.ts
+++ app/index.ts
@@ -1,26 +1,29 @@
 /// <reference types="electron" />
 import { app, BrowserWindow, Menu, Tray, autoUpdater, dialog } from "electron";
-
+import * as child_process from "child_process";
+import * as path from "path";
+import * as url from "url";
+const spawn = child_process.spawn;
 const version = require("../package.json").version;
-
 
 // set application title
 let appTitle = `Desktop Grimoire v${version}`;
+let win;
 app.setName(appTitle);
 
-
-
-app.on("ready", () => {
-    let mainWindow = new BrowserWindow({
+function createWindow() {
+    win = new BrowserWindow({
         width: 1000,
         height: 750,
         backgroundColor: "#2e2c29",
         title: app.getName()
     });
 
-    mainWindow.once("ready-to-show", () => {
-        mainWindow.loadURL("localhost:4000");
-        mainWindow.show();
-        mainWindow.focus();
-    });
-});
+    win.loadURL(url.format({
+        pathname: "localhost:4000",
+        protocol: "http:",
+        slashes: true
+    }));
+}
+
+app.on("ready", createWindow);
